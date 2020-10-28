f9607a364b9b873e870fac870bed25d4374d324e
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-10-27 20:03:41 -0500

    added x86_64 SIMD mul

--- src/simd-math-x86_64.c
+++ src/simd-math-x86_64.c
@@ -175,6 +175,49 @@ void gauss_floordiv_double_array(
     }
 }
 
+void gauss_mul_double_array(
+    double *result,
+    const double *a,
+    const double *b,
+    size_t size
+) {
+    size_t i = 0;
+
+#ifdef __AVX512F__
+    /* AVX-512 */
+    for(; i < (size & ~0x7); i += 8) {
+        const __m512d kA8 = _mm512_load_pd(&a[i]);
+        const __m512d kB8 = _mm512_load_pd(&b[i]);
+        const __m512d kRes = _mm512_mul_pd(kA8, kB8);
+        _mm512_stream_pd(&result[i], kRes);
+    }
+#endif
+
+#ifdef __AVX__
+    /* AVX loop */
+    for (; i < (size & ~0x3); i += 4) {
+        const __m256d kA4 = _mm256_load_pd(&a[i]);
+        const __m256d kB4 = _mm256_load_pd(&b[i]);
+        const __m256d kRes = _mm256_mul_pd(kA4, kB4);
+        _mm256_stream_pd(&result[i], kRes);
+    }
+#endif
+
+#ifdef __SSE2__
+    /* SSE2 loop */
+    for (; i < (size & ~0x1); i += 2) {
+        const __m128d kA2 = _mm_load_pd(&a[i]);
+        const __m128d kB2 = _mm_load_pd(&b[i]);
+        const __m128d kRes = _mm_mul_pd(kA2, kB2);
+        _mm_stream_pd(&result[i], kRes);
+    }
+#endif
+    /* Serial loop */
+    for(; i < size; i++) {
+        result[i] = a[i] + b[i];
+    }
+}
+
 void gauss_add_double_array(
     double *result,
     const double *a,
