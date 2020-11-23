c0222579dd6714d9ec89b0122598c2264b7ec036
kkloberdanz <kyle.g.kloberdanz@gmail.com>
2020-11-23 16:02:25 -0600

    cleanup

--- src/util.c
+++ src/util.c
@@ -2,6 +2,7 @@
 
 #include "vec-math.h"
 #include "util.h"
+#include "alloc.h"
 
 void *aligned_alloc(size_t, size_t);
 
@@ -11,16 +12,4 @@ void *gauss_simd_alloc(size_t size) {
 #else
     return malloc(size);
 #endif
-}
-
-double gauss_double_array_at(const double *arr, size_t i) {
-    return arr[i];
-}
-
-void gauss_set_double_array_at(double *arr, size_t i, double value) {
-    arr[i] = value;
-}
-
-void gauss_set_float_array_at(float *arr, size_t i, float value) {
-    arr[i] = value;
 }
